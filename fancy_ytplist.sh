024e707d0b0000a026af34687b28e62f2dc45c70
Caitlin "Cat" Chapman <12089454+CatChapman@users.noreply.github.com>
2020-12-04 20:06:49 -0500

    Update fancy_ytplist.sh

--- fancy_ytplist.sh
+++ fancy_ytplist.sh
@@ -6,15 +6,18 @@ youtube-dl -s ${1} >> tempID.txt
 #silent youtube-dl dump into a tempID.txt file, for use by getpldir.sh to extract the playlist name and subsequently the directory name
 
 dir=$(./getpldir.sh)
-#using "update.sh" as a var seems to play nicer, idk if it's really necessary but...?
-script="update.sh"
-#have to put the trailing / in the var itself otherwise it complains
-dir="$dir/"
+#invoking the getpldir.sh script to get the playlist directory name
 
+script="update.sh"
+#using "update.sh" as a var seems to play nicer, idk if it's really necessary but...?
+
+dir="$dir/"
+#have to put the trailing / in the var itself otherwise it complains
 
 rm tempID.txt
-#have to put quotes around the variables when called otherwise it complains
+#removing the temp file otherwise things get wonky.
 echo "#!/bin/bash" >> "$dir""$script"
+#have to put quotes around the variables when called otherwise it complains
 
 echo "youtube-dl -f ‘bestaudio’ -i -o '%(playlist_index)s - %(title)s.%(ext)s' https://www.youtube.com/playlist?list=${1}" >> "$dir""$script"
 chmod +x "$dir""$script"
