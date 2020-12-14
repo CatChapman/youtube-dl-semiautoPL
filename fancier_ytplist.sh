e58f43ff836a5990b563305287a0a6a5de5274b1
Cat Chapman <12089454+CatChapman@users.noreply.github.com>
2020-12-13 21:20:01 -0500

    Update fancier_ytplist.sh

--- fancier_ytplist.sh
+++ fancier_ytplist.sh
@@ -17,7 +17,7 @@ rm tempID.txt
 
 #staging for m3u playlist generation
 now=$(date "+%Y-%m-%d") #timestamps are cool and good
-cd $dir #changing to newly created directory because it's simpler this way
+cd "$dir" #changing to newly created directory because it's simpler this way
 
 ls | grep -v .sh | grep -v .m3u >> $now"_"$plname.m3u #ls piped thru inverse grep works better than plain ls I guess? macOS ships with BSD ls, not GNU ls; no inverse filter with BSD ls.
 
