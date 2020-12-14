130019177e0ad7435c200a4e677e88cd5bf8f69d
Cat Chapman <12089454+CatChapman@users.noreply.github.com>
2020-12-13 21:49:35 -0500

    Update fancier_ytplist.sh
    fixed some unwanted behavior with spaces in file and directory names. should be good now!

--- fancier_ytplist.sh
+++ fancier_ytplist.sh
@@ -1,4 +1,4 @@
-!/bin/bash
+#!/bin/bash
 
 youtube-dl -f ‘bestaudio’ -i -o '%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s' ${1}
 
@@ -19,10 +19,10 @@ rm tempID.txt
 now=$(date "+%Y-%m-%d") #timestamps are cool and good
 cd "$dir" #changing to newly created directory because it's simpler this way
 
-ls | grep -v .sh | grep -v .m3u >> $now"_"$plname.m3u #ls piped thru inverse grep works better than plain ls I guess? macOS ships with BSD ls, not GNU ls; no inverse filter with BSD ls.
+ls | grep -v .sh | grep -v .m3u >> "$now"_"$plname".m3u #ls piped thru inverse grep works better than plain ls I guess? macOS ships with BSD ls, not GNU ls; no inverse filter with BSD ls.
 
-#test for the presence of update.sh in the target (now working) directory
-#otherwise if update.sh exists, echo will append redundant lines into the file. not desired behavior.
+#ls *.webm >> $now.m3u
+#ls *.m4a >> $now.m3u
 
 if test -f "$script"; then
   echo "$script already exists in the target directory."
@@ -33,7 +33,7 @@ if test -f "$script"; then
       echo "#!/bin/bash" >> "$script";
       echo "youtube-dl -f ‘bestaudio’ -i -o '%(playlist_index)s - %(title)s.%(ext)s' ${1}" >> "$script";
       echo "now=\$(date \"+%Y-%m-%d\")" >> "$script";
-      echo "ls | grep -v .sh | grep -v .m3u >> \$now\"_\"$plname.m3u" >> "$script";
+      echo "ls | grep -v .sh | grep -v .m3u >> \$now\_\""$plname"\".m3u" >> "$script";
       #so now the update script will also build a new playlist with each run
       chmod +x "$script";
       echo "$script has been written in $(pwd). Run $script within $(pwd) to update the files.";
@@ -44,18 +44,18 @@ if test -f "$script"; then
     esac
   done
 
-else #behavior for when there is no update.sh present in the target (again, now working) directory
+else
   echo "#!/bin/bash" >> "$script"
 
   echo "youtube-dl -f ‘bestaudio’ -i -o '%(playlist_index)s - %(title)s.%(ext)s' ${1}" >> "$script"
   echo "now=\$(date \"+%Y-%m-%d\")" >> "$script"
-  echo "ls | grep -v .sh | grep -v .m3u >> \$now\"_\"$plname.m3u" >> "$script"
+  echo "ls | grep -v .sh | grep -v .m3u >> \$now\_\""$plname"\".m3u" >> "$script"
   #so now the update script will also build a new playlist with each run
   chmod +x "$script"
   echo "$script has been written in $(pwd). Run $script within $(pwd) to update the files."
 fi
 
-#this is probably pretty sloppy, but I didn't go to computer college. sorry :)
+#this is sloppy but I didn't go to computer college, sorry :)
 
 #shoutouts:
 # https://stackoverflow.com/questions/226703/how-do-i-prompt-for-yes-no-cancel-input-in-a-linux-shell-script
